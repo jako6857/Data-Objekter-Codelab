c333878b02818a23d2e82d08473ac8ebf18016b6
jako6857 <82064889+jako6857@users.noreply.github.com>
2025-08-26 10:54:08 +0200

    medium opgave 1

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -1,13 +1,22 @@
 /* Opgave 1*/
+const data ={
+    title: "opgaver",
+    items: ["opgave 1","opgave 2","opgave 3","opgave 4", "opgave 5"]
+};
+
+data.items.push("opgave 6"); 
+console.log(data);
+
 console.group("opgave 1");
-
-
-
 console.groupEnd();
 
 
 
 /*opgave 2*/
+
+delete data.jobTitle;
+console.log(data);
+
 console.group("opgave 2");
 
 
