7666c4e26cc14ef1acef6bb7815204621d35cf59
jako6857 <82064889+jako6857@users.noreply.github.com>
2025-08-26 11:21:53 +0200

    rød opgave 1 & 2

--- opgavefiler/js/hard.js
+++ opgavefiler/js/hard.js
@@ -1,6 +1,33 @@
 /* opgave 1 */
 
+const todoApp = {
+            shopping: {
+            name: "Indkøb",
+            items: [
+            { text: "Køb mælk", done: false },
+            { text: "Køb brød", done: true }
+            ]
+            },
+            school: {
+            name: "Skolearbejde",
+            items: [
+            { text: "Læs kapitel 3", done: false },
+            { text: "Skriv rapport", done: true }
+            ]
+            }
+            };
 
+
+            function addItem(listKey, itemText) {
+            const list = todoApp[listKey];
+            if (list) { 
+                const newItem = {text: itemText, done: false };
+                list.items.push(newItem);
+             }
+                console.log(todoApp);
+                }
+
+                addItem("shopping", "Køb smør");
 
 console.group("opgave 1");
 
