fb4f9e788bc0a8cede9c284a281b207acfeeb4a4
jako6857 <82064889+jako6857@users.noreply.github.com>
2025-08-26 10:29:32 +0200

    alle 3 grønne opgaver

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,9 +1,21 @@
 /* opgave 1 */
+
+const person ={
+    firstName: "Jakob", 
+    age: 24, 
+    jobTitle: "student"};
+    console.log(person);
+
 console.group("opgave 1");
 
 console.groupEnd();
 
 /* opgave 2 */
+
+console.log(person.firstName);
+console.log(person.age);    
+console.log(`${person.firstName} arbejder som ${person.jobTitle}`);
+
 console.group("opgave 2");
 
 
@@ -11,6 +23,10 @@ console.group("opgave 2");
 console.groupEnd();
 
 /* opgave 3 */
+
+delete person.age;
+console.log(person);
+
 console.group("opgave 3");
 
 
